96ef67d8c74e5f9a8943447de3d52fa2be07b2bf
Pim Rutgers <pim.rutgers@gmail.com>
2015-05-13 16:26:02 +0200

    Make users life easy by linking programmer to board
    No mare bricks please thanks @Salandora for testing and giving the
    heads up

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Select a hex file, port, baudrate and board and press flash.
 
 This project is in heavy *WIP* phase. So please be carefull. Would be nice to run octoprint in --debug mode to see whats going on. 
 
+At the moment a limit amount of boards is supported. If you know your board + programmer for AVR dude please make a PR with the information so I can add it to the plugin. 
+
+-Booli
+
--- octoprint_flasharduino/static/js/flasharduino.js
+++ octoprint_flasharduino/static/js/flasharduino.js
@@ -10,40 +10,18 @@ $(function() {
         self.hex_path = ko.observable(undefined);
         self.selected_port = ko.observable(undefined);
         self.selected_baudrate = ko.observable(undefined);
-        self.selected_board = ko.observable(undefined);
+        self.selected_board = ko.observableArray();
         self.selected_programmer = ko.observable(undefined);
 
         self.flash_button = $("#settings-flash-arduino-start");
         self.upload_hex = $("#settings-flash-arduino");
 
-        // Arrays of options
-        self.programmers = [
-            {"value": "arduino", "text": gettext("Arduino programmer")},
-            {"value": "avr910", "text": gettext("AVR910")},
-            {"value": "butterfly", "text": gettext("Atmel Butterfly evaluation board; Atmel AppNotes AVR109, AVR911")},
-            {"value": "butterfly_mk", "text": gettext("Mikrokopter.de Butterfly")},
-            {"value": "par", "text": gettext("Parallel port bitbanging")},
-            {"value": "stk500", "text": gettext("Atmel STK500 Version 1.x firmware")},
-            {"value": "stk500generic", "text": gettext("Atmel STK500, autodetect firmware version")},
-            {"value": "stk500v2", "text": gettext("Atmel STK500 Version 2.x firmware")},
-            {"value": "stk500hvsp", "text": gettext("Atmel STK500 V2 in high-voltage serial programming mode")},
-            {"value": "stk500pp", "text": gettext("Atmel STK500 V2 in parallel programming mode")},
-            {"value": "stk600", "text": gettext("Atmel STK600")},
-            {"value": "stk600hvsp", "text": gettext("Atmel STK600 in high-voltage serial programming mode")},
-            {"value": "stk600pp", "text": gettext("Atmel STK600 in parallel programming mode")},
-            {"value": "wiring", "text": gettext("Wiring. Basically STK500v2 protocol, with some glue to trigger the bootloader")}
-        ];
-
 
         self.boards = [
-            {"value": "usb1286", "text": gettext("AT90USB1286 Teensy++ ")},
-            {"value": "m1280", "text": gettext("ATmega1280 Arduino Mega")},
-            {"value": "m2560", "text": gettext("ATmega2560 Leapfrog CreatrHS, Ultimaker, RAMPS 1.3")},
-            {"value": "m644 ", "text": gettext("ATmega644 SanguinoA")},
-            {"value": "m644p", "text": gettext("ATmega644P Sanguino")},
-            {"value": "m1284p", "text": gettext("ATmega1284P Sanguino")},
-            {"value": "m328p", "text": gettext("ATmega328P Duemilanove")},
-            {"value": "usb646", "text": gettext("AT90USB646 Brainwave")}
+            {"value": {"board": "m2560", "programmer": "wiring"}, "text": gettext("ATmega2560 Leapfrog CreatrHS, Ultimaker 2, RAMPS 1.3")},
+            {"value": {"board": "m1280", "programmer": "arduino"}, "text": gettext("ATmega1280 Arduino Mega")},
+            {"value": {"board": "m328p", "programmer": "arduino"}, "text": gettext("Duemilanove /w ATmega328")},
+            {"value": {"board": "atmega168", "programmer": "arduino"}, "text": gettext("Duemilanove /w ATmega168")}
         ];
 
         self.upload_hex.fileupload({
@@ -58,8 +36,8 @@ $(function() {
                 self.flash_button.unbind("click");
                 self.flash_button.on("click", function() {
                     var flash_data = {
-                        board: self.selected_board(),
-                        programmer: self.selected_programmer(),
+                        board: self.selected_board().board,
+                        programmer: self.selected_board().programmer,
                         port: self.selected_port(),
                         baudrate: self.selected_baudrate()
                     }; 
