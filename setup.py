e9c1db3f8b87d9a49ed28e7de193346222485954
Pim Rutgers <pim.rutgers@gmail.com>
2017-02-02 16:07:41 +0100

    forgot requires

--- setup.py
+++ setup.py
@@ -34,6 +34,14 @@ plugin_url = ""
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
 
+# Any additional requirements besides OctoPrint should be listed here
+
+plugin_requires = []
+
+### --------------------------------------------------------------------------------------------------------------------
+### More advanced options that you usually shouldn't have to touch follow after this point
+### --------------------------------------------------------------------------------------------------------------------
+
 # Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
 # already be installed automatically if they exist.
 plugin_additional_data = []
